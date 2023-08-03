762b60de0db822791ab3d9acc479cc3a1fb7810e
Nicolas Riousset <nicolas@riousset.com>
2023-08-03 15:25:37 +0200

    Suppression test obsolète

--- src/test/java/fr/neolegal/fec/FecReaderTest.java
+++ src/test/java/fr/neolegal/fec/FecReaderTest.java
@@ -2,18 +2,10 @@ package fr.neolegal.fec;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 import org.junit.jupiter.api.Test;
-
-import fr.neolegal.fec.liassefiscale.Repere;
 
 public class FecReaderTest {
 
@@ -35,28 +27,5 @@ public class FecReaderTest {
         assertEquals(NatureAnomalie.LIGNES_VIDES, fec.getAnomalies().get(0).getNature());
         assertEquals(10756, fec.getAnomalies().get(0).getValeur());
 
-    }
-
-    @Test
-    void test() {
-        Path path = Path.of("d:/tmp/regles-calcul-reperes.csv");
-
-        try (FileWriter fileWriter = new FileWriter(path.toString());
-                CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.MYSQL )) {
-
-            // Écriture de l'en-tête du fichier CSV
-            csvPrinter.printRecord("Repère", "Formulaire", "Intitulé", "Règle de calcul");
-
-            List<Repere> reperes = new LinkedList<>(Repere.DEFINITIONS.values());
-            Collections.sort(reperes);
-            for (Repere repere : reperes) {
-                csvPrinter.printRecord(repere.getRepere(), repere.getFormulaire(), repere.getNom(), repere.getExpression());
-            }
-
-            System.out.println("Fichier CSV créé avec succès !");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
     }
 }
