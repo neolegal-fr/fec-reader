badeb00d5821dd084df0b2f8f5248d1cee4a1202
Nicolas Riousset <nicolas@riousset.com>
2023-08-05 18:12:06 +0200

    Ajout d'un second fichier pour les tests de calcul des repères de la liasse fiscale

--- src/test/java/fr/neolegal/fec/liassefiscale/LiasseFiscaleHelperTest.java
+++ src/test/java/fr/neolegal/fec/liassefiscale/LiasseFiscaleHelperTest.java
@@ -10,17 +10,26 @@ import fr.neolegal.fec.FecHelper;
 
 public class LiasseFiscaleHelperTest {
 
-    LiasseFiscale liasse = LiasseFiscaleHelper
+    LiasseFiscale liasse1 = LiasseFiscaleHelper
             .buildLiasseFiscale(FecHelper.read(Path.of("target/test-classes/123456789FEC20500930.txt")));
+    LiasseFiscale liasse2 = LiasseFiscaleHelper
+            .buildLiasseFiscale(FecHelper.read(Path.of("target/test-classes/000000000FEC20231231.txt")));
 
     @Test
     void buildLiasseFiscale() {
-        assertEquals(liasse.getFormulaires().size(), Formulaire.values().length);
+        assertEquals(liasse1.getFormulaires().size(), Formulaire.values().length);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/123456789FEC20500930-expected.csv")
-    void getMontant(String repere, Double expectedValue) {
-        assertEquals(expectedValue, liasse.getMontant(repere).get());
+    void getMontant_liasse1(String repere, Double expectedValue) {
+        assertEquals(expectedValue, liasse1.getMontant(repere).get());
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/000000000FEC20231231-expected.csv")
+    void getMontant_liasse2(String repere, Double expectedValue) {
+        assertEquals(expectedValue, liasse2.getMontant(repere).get());
+    }
+
 }
